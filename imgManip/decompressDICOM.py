91c8c922b4c6649bd4074ea8017fb72e9d883fc3
Alberta <alberta@pc37.md.ucalgary.ca>
2019-07-09 10:55:58 -0600

    Added error checks and transfer syntax check

--- imgManip/decompressDICOM.py
+++ imgManip/decompressDICOM.py
@@ -1,12 +1,12 @@
 # coding=utf-8
-#----------------------------------------------------- 
+#-----------------------------------------------------
 # decompressDICOM.py
 #
 # Created by:   Michael Kuczynski
 # Created on:   2019.07.07
 #
 # Description: Decompresses DICOM images.
-#----------------------------------------------------- 
+#-----------------------------------------------------
 #
 # Requirements:
 #   -Python 3.4 or later
@@ -18,12 +18,14 @@
 # Notes:
 #   -The decompressed DICOM directory it will be created automatically
 #    in the compressed DICOM folder and will be named: decompressedDICOMs
-#----------------------------------------------------- 
+#-----------------------------------------------------
 
 import pydicom
 import os
 import sys
 import errno
+
+from pydicom.errors import InvalidDicomError
 
 inputPath = sys.argv[1]
 
@@ -46,15 +48,29 @@ except OSError as e:
 # Loop through all DICOMs in the provided directory, rename and decompress
 for file in os.listdir(inputPathAbs):
     filename = os.fsdecode(file)
-    
-    currentFilePath = os.path.join(inputPathAbs, filename)
 
-    # TO-DO:
-    # Check transfer syntax tag in DICOM header to see if file is compressed or not
-    # If the file is already decompressed, skip it
+    currentFilePath = os.path.join(inputPathAbs, filename)
 
     # Check if we have a file
     if os.path.isfile(currentFilePath):
+
+        # Read the DICOM file. Make sure we have a valid DICOM file...
+        try:
+            dicom = pydicom.dcmread(currentFilePath)
+        except InvalidDicomError:
+            print ("File: " + currentFilePath + " is not a valid DICOM file. Skipping...")
+            continue
+
+        # Check transfer syntax tag in DICOM header to see if file is compressed or not
+        # If the file is already decompressed, skip it
+        tsUID = dicom.file_meta.TransferSyntaxUID
+
+        # Uncompressed Implicit VR Little-endian = 1.2.840.10008.1.2
+        # Uncompressed Explicit VR Little-endian = 1.2.840.10008.1.2.1
+        # Uncompressed Explicit VR Big-endian = 1.2.840.10008.1.2.2
+        if (tsUID == "1.2.840.10008.1.2") or (tsUID == "1.2.840.10008.1.2.1") or (tsUID == "1.2.840.10008.1.2.2"):
+            print ("File " + filename + " is not compressed...")
+            continue
 
         # Rename the DICOM file to include a file extension (if needed)
         if "." not in filename:
@@ -62,12 +78,11 @@ for file in os.listdir(inputPathAbs):
 
             print ("Decompressing file: " + filename)
 
-            ds = pydicom.dcmread(currentFilePath)
-            ds.decompress()
+            dicom.decompress()
 
             # Save the decompressed file
             try:
-                ds.save_as(outputFilePath)
+                dicom.save_as(outputFilePath)
             except OSError as e:
                 if e.errno != errno.ENOENT:     # No such file or directory error
                     print ("ERROR: No such file or directory named " + outputFilePath)
@@ -79,12 +94,11 @@ for file in os.listdir(inputPathAbs):
 
             print ("Decompressing file: " + filename)
 
-            ds = pydicom.dcmread(currentFilePath)
-            ds.decompress()
+            dicom.decompress()
 
             # Save the decompressed file
             try:
-                ds.save_as(outputFilePath)
+                dicom.save_as(outputFilePath)
             except OSError as e:
                 if e.errno != errno.ENOENT:     # No such file or directory error
                     print ("ERROR: No such file or directory named " + outputFilePath)
