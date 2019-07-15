3b3a49b7f2b6a937fdf49b11cfe65bd06ba7ec4a
Michael Kuczynski <mkuczyns@ucalgary.ca>
2019-07-15 11:56:18 -0600

    Changed comments

--- imgManip/decompressDICOM.py
+++ imgManip/decompressDICOM.py
@@ -32,16 +32,16 @@ from pydicom.errors import InvalidDicomError
 # Just for fun :)
 # Print iterations progress
 def print_progress(iteration, total, prefix='', suffix='', decimals=1, bar_length=100):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        bar_length  - Optional  : character length of bar (Int)
-    """
+    #
+    # Call in a loop to create terminal progress bar
+    # @params:
+    #     iteration   - Required  : current iteration (Int)
+    #     total       - Required  : total iterations (Int)
+    #     prefix      - Optional  : prefix string (Str)
+    #     suffix      - Optional  : suffix string (Str)
+    #     decimals    - Optional  : positive number of decimals in percent complete (Int)
+    #     bar_length  - Optional  : character length of bar (Int)
+    #
     str_format = "{0:." + str(decimals) + "f}"
     percents = str_format.format(100 * (iteration / float(total)))
     filled_length = int(round(bar_length * iteration / float(total)))
